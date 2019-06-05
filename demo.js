7ae81a6cc67174a07a984ff69e13322f52a84c30
sangeeth98 <sangeethkumarvm@gmail.com>
2019-06-05 19:45:21 +0530

    commit-1

---
+++ demo.js
@@ -0,0 +1,12 @@
+document.write("<h1>Hello World!</h1>");
+//semi-colon's separate lines or statements. ';' represents the end of statement
+document.write("<h2>Telusko Learning<h2>");
+// This is single line comment
+/* This is multiline comment
+
+In this program we are printing a hello world comment
+And when we are writing multiple lines of code and make mistake
+We can comment those lines together and save them later for reference
+and learn where we went wrong with the code and how to reuse them
+It won't affect the programming.
+*/
